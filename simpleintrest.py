590fbb086b4d9b964b87b5fb7dbd94cbbfa88e42
ramyachowdary97 <112850276+ramyachowdary97@users.noreply.github.com>
2022-09-05 19:36:19 +0530

    Add files via upload

---
+++ simpleintrest.py
@@ -0,0 +1,8 @@
+#k ramya chowdary (192011288)
+principle_amt=float(input("enter the principle amount"))
+roi=float(input("enter the rate of intrest"))
+time=float(input("enter the time taken"))
+si=(principle_amt*time*roi)/100
+print(si)
+
+
