8012a2bb98b7ccc65039fe0dc9dac427b125c9d0
ramyachowdary97 <112850276+ramyachowdary97@users.noreply.github.com>
2022-09-06 11:15:25 +0530

    Add files via upload

---
+++ multiplicationtable.py
@@ -0,0 +1,6 @@
+#K RAMYA CHOWDARY (192011288)
+num= int(input ("Enter the number of which the user wants to print the multiplication table: "))            
+print ("The Multiplication Table of: ",num)    
+for count in range(1, 11):      
+ print (num, 'x', count, '=', num * count)
+ 
