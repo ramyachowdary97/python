e55c9f3b1e72c6beda362181b9d6a26cc9a41890
ramyachowdary97 <112850276+ramyachowdary97@users.noreply.github.com>
2022-09-06 11:13:05 +0530

    Add files via upload

---
+++ greatestofthree.py
@@ -0,0 +1,13 @@
+n1 = int(input(" enter the first number "))
+n2 = int(input(" enter the second number "))
+n3 = int(input(" enter the third number "))
+if(n1>n2)and (n1>n3):
+           print("n1 is greater")
+elif(n2>n1) and (n2>n3):
+      print("n2 is greater")
+elif(n3>n1) and (n3>n2):
+      print(" n3 is greater")
+else:
+    print("all are equal")
+
+            
