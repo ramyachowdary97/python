05060e489cbf59009b2652de1c4a12b9fe2f7670
ramyachowdary97 <112850276+ramyachowdary97@users.noreply.github.com>
2022-09-06 13:53:29 +0530

    Add files via upload

---
+++ evenoroddusingfunctions.py
@@ -0,0 +1,13 @@
+def num(n):
+    if(n%2==0):
+        print("given number is even number")
+    else:
+        print("given number is odd number")
+    return num
+
+n=int(input("enter a number"))
+number=num(n)
+print(number)
+
+
+
