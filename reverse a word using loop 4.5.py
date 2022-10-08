b9aabfae4c47638dff9303ba0e33c2fcc3b60e85
K RAMYA CHOWDARY <112850276+ramyachowdary97@users.noreply.github.com>
2022-10-08 09:36:24 +0530

    Add files via upload

---
+++ reverse a word using loop 4.5.py
@@ -0,0 +1,8 @@
+def reverse_string(str):  
+    str1 = ""
+    for i in str:  
+        str1 = i + str1  
+    return str1    
+str = input("enter any string:")       
+print("The original string is: ",str)  
+print("The reverse string is",reverse_string(str))
